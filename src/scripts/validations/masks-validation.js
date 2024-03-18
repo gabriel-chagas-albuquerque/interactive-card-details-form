03d5fbb839ad343e96ba827fcd02cbcedab3e33d
Gabriel Albuquerque <gabrieldafortaleza0100@gmail.com>
2024-03-18 16:12:20 -0300

    página com cartão de crédito interativo

--- src/scripts/validations/masks-validation.js
+++ src/scripts/validations/masks-validation.js
@@ -1,5 +1,5 @@
 function maskValidationNumberCard(inputItens) {
-  inputItens[1].addEventListener("keydown", () => {
+  inputItens[1].addEventListener("keypress", () => {
     const inputLength = inputItens[1].value.length;
     if (inputLength === 4 || inputLength === 9 || inputLength === 14) {
       inputItens[1].value += " ";
@@ -9,62 +9,15 @@ function maskValidationNumberCard(inputItens) {
 
 function charactersValidation(inputItens) {
   inputItens.forEach((input, i) => {
-    input.addEventListener("keydown", (e) => {
+    input.addEventListener("keypress", (e) => {
       const char = String.fromCharCode(e.keyCode || e.which);
-      
-      const pattern = ["[0-9]", "[0-9]", "[0-9]", "[0-9]"];
-      if (i === 0) {
-        if (
-          (e.keyCode >= 65 && e.keyCode <= 90) ||
-          (e.which >= 65 && e.which <= 90) ||
-          e.keyCode === 8 ||
-          e.keyCode === 9 ||
-          e.keyCode === 16 ||
-          e.keyCode === 17 ||
-          e.keyCode === 18 ||
-          e.keyCode === 20 ||
-          e.keyCode === 32 ||
-          e.keyCode === 37 ||
-          e.keyCode === 39 || 
-          e.which === 8 ||
-          e.which === 9 ||
-          e.which === 16 ||
-          e.which === 17 ||
-          e.which === 18 ||
-          e.which === 20 ||
-          e.which === 32 ||
-          e.which === 37 ||
-          e.which === 39
-          ) 
-        {
+      const pattern = ["[a-zA-Z ]", "[0-9]", "[0-9]", "[0-9]", "[0-9]"];
+        if (char.match(pattern[i])) {
         } else {
           e.preventDefault();
         }
-      } else {
-        if (
-          char.match(pattern[i-1]) ||
-          e.keyCode === 8 ||
-          e.keyCode === 9 ||
-          e.keyCode === 16 ||
-          e.keyCode === 17 ||
-          e.keyCode === 18 ||
-          e.keyCode === 20 ||
-          e.keyCode === 37 ||
-          e.keyCode === 39 ||
-          e.which === 8 ||
-          e.which === 9 ||
-          e.which === 16 ||
-          e.which === 20 ||
-          (e.keyCode >= 96 && e.keyCode <= 105) ||
-          (e.which >= 96 && e.which <= 105) ||
-          e.which === 37 ||
-          e.which === 39
-        ) {
-        } else {
-          e.preventDefault();
-        }
-      }
+      })
     });
-  });
-}
+  };
+
 export { maskValidationNumberCard, charactersValidation };
